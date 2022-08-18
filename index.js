048ff5e9c28e0983a9326aa0ee4af8dedfcd692d
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-08-18 23:57:36 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(fun = "roller-skate") {
+    return (`This Saturday, I want to ${fun}!`);
+
+}
+saturdayFun();
+const mondayWork = function (work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(flair) {
+    const innerFunction = function (msg = "special") {
+        return 'You are ${flair}${msg}${flair}!';
+    };
+    return innerFunction;
+}
+wrapAdjective("*");
+wrapAdjective("%")("dedicated programmer");   
